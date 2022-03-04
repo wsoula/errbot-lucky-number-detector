8c2fad982816a0cfb475a590c4f3b538d94ddaa0
Will Soula <spam@wsoula.info>
2022-03-04 15:42:40 -0700

    feat: Only run for one prefix like a prod

--- luckynumberdetector.py
+++ luckynumberdetector.py
@@ -5,6 +5,7 @@ from errbot import BotPlugin, botcmd
 
 DEFAULT_LUCKY_NUMBERS = '77777977,777797777,777'
 DEFAULT_MINIMUM_NUMBERS = '4'
+DEFAULT_PREFIX_TO_MONITOR = '!'
 
 
 class Luckynumberdetector(BotPlugin):
@@ -27,14 +28,16 @@ class Luckynumberdetector(BotPlugin):
         reply = reply + ' = ' + str(total)
         match = [int(i) for i in match]
         if sum(match) in lucky_numbers and count >= minimum_numbers:
-            self.send_card(
-                  in_reply_to=mess,
-                  body=reply,
-                  title='The numbers in your message add up to the lucky number: ' + str(total)
-              )
+            prefix_to_monitor = os.getenv('prefix_to_monitor', DEFAULT_PREFIX_TO_MONITOR)
+            prefix = os.getenv('BOT_PREFIX', '!')
+            if prefix == prefix_to_monitor:
+                self.send_card(in_reply_to=mess, body=reply,
+                               title='The numbers in your message add up to the lucky number: ' + str(total))
 
     @botcmd()
     def luckynumber(self, msg, args):
+        """ Return what the luck number is """
+        self.log.info('msg=%s args=%s', msg, args)
         lucky_numbers = os.getenv('lucky_numbers', DEFAULT_LUCKY_NUMBERS).split(',')
         minimum_numbers = int(os.getenv('minimum_numbers', DEFAULT_MINIMUM_NUMBERS))
-        return str(lucky_numbers) + ' in ' + str(minimum_numbers) + ' numbers'
+        return str(lucky_numbers) + ' in a minimum of ' + str(minimum_numbers) + ' numbers'
